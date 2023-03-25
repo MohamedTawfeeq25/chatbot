1fa4801be1851c1455e8e034429d15f82f989c5c
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-03-25 17:48:09 +0530

    basic command added

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+var data={
+    "how old are you?":"As an AI chatbot, I don't have an age like humans do. How may I assist you?",
+    "thank you":"You're welcome! Is there anything else I can assist you with?",
+    "hi": "Hello! How can I assist you today?",
+    "what is your name?": "My name is ChatBOT. How may I assist you?",
+    "can you tell me a joke?":"Sure! Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "what is the meaning of life?":"The meaning of life is a subjective topic that can vary from person to person. What do you think is the meaning of life?",
+    "what can you do?":"I can help you with a variety of tasks, such as finding information, setting reminders, making reservations, and more. What can I assist you with today?",
+    "how are you?": "As an AI chatbot, I don't have feelings like humans do. How may I assist you?",
+    "what is your favorite color?": "As an AI chatbot, I don't have personal preferences like humans do. How may I assist you?"
+    
+}
 document.addEventListener("keypress",(e)=>{if(e.key=='Enter'){send()}});
 
-function Reply(){
+function Reply(command){
     var cont=document.getElementById('chatbox');
     var box=document.createElement('div');
     box.setAttribute("id","bcmd");
     var text=document.createElement('div');
     text.setAttribute("id","botc");
-    text.textContent="Hello";
+    if(data[command]!=undefined){
+        text.textContent=data[command];
+    }
+    else{
+        text.textContent="I'm sorry, I don't have the answer to that. Can you please ask me something else?";
+    }
     var imgc=document.createElement('div');
     imgc.setAttribute("id","botimg");
     var img=document.createElement('img');
@@ -17,7 +34,7 @@ function Reply(){
     cont.appendChild(box);
     var div=document.getElementById('chat');
     div.scrollTop=div.scrollHeight;
-    
+    document.getElementById('command').value="";
 }
 function send(){
     var cmd=document.getElementById("command").value;
@@ -30,7 +47,8 @@ function send(){
         text.textContent=cmd;
         box.appendChild(text);
         cont.appendChild(box);
-        Reply()
+        
+        Reply(cmd)
     }
    else{
        alert('write any command');
