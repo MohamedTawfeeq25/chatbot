da240107a42d8f98170eab0075823cefa634af80
SMT <mohamedtawfeeqprogrammer25@gmail.com>
2023-03-26 12:14:59 +0530

    added clear command and case insensitive update

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ fetch('./command.json')
 document.addEventListener("keypress",(e)=>{if(e.key=='Enter'){send()}});
 
 function Reply(command){
+    if(command=="clear"){
+        document.getElementById('chatbox').innerHTML="";
+    }
+    else{
     var cont=document.getElementById('chatbox');
     var box=document.createElement('div');
     box.setAttribute("id","bcmd");
     var text=document.createElement('div');
     text.setAttribute("id","botc");
+   
     if(data[command]!=undefined){
         text.textContent=data[command];
     }
@@ -28,10 +33,12 @@ function Reply(command){
     cont.appendChild(box);
     var div=document.getElementById('chat');
     div.scrollTop=div.scrollHeight;
+    }
     document.getElementById('command').value="";
 }
 function send(){
-    var cmd=document.getElementById("command").value;
+    var cd=document.getElementById("command").value;
+    var cmd=cd.toLowerCase();
     if(cmd!=""){
         var cont=document.getElementById('chatbox');
         var box=document.createElement('div');
